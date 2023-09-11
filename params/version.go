ca35df7482589ab73a6ebea13de45cac51e53c48
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2023-09-11 09:05:10 -0400

    activate max(prio)FeePerGas in firehose context
    also:
    * bump version to fh2.3-2
    * make github docker build branches

--- params/version.go
+++ params/version.go
@@ -21,10 +21,10 @@ import (
 )
 
 const (
-	VersionMajor = 0       // Major version component of the current release
-	VersionMinor = 4       // Minor version component of the current release
-	VersionPatch = 0       // Patch version component of the current release
-	VersionMeta  = "fh2.3" // Version metadata to append to the version string
+	VersionMajor = 0         // Major version component of the current release
+	VersionMinor = 4         // Minor version component of the current release
+	VersionPatch = 0         // Patch version component of the current release
+	VersionMeta  = "fh2.3-2" // Version metadata to append to the version string
 
 	FirehoseVersionMajor = 2
 	FirehoseVersionMinor = 3
