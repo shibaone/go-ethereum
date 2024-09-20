c9faecf4c0795c204f2fe97846628d25055758a3
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2024-09-20 11:43:05 -0400

    fixing libnum not moving when optimism node started in snapshot mode from bedrock

--- eth/tracers/firehose.go
+++ eth/tracers/firehose.go
@@ -1514,6 +1514,10 @@ func (f *Firehose) printBlockToFirehose(block *pbeth.Block, finalityStatus *Fina
 		}
 	}
 
+	if block.Number-libNum >= 200 {
+		libNum = block.Number - 200
+	}
+
 	// **Important* The final space in the Sprintf template is mandatory!
 	f.outputBuffer.WriteString(fmt.Sprintf("FIRE BLOCK %d %s %d %s %d %d ", block.Number, hex.EncodeToString(block.Hash), previousNum, previousHash, libNum, block.Time().UnixNano()))
 
