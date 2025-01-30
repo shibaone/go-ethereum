7aa8b99630a4634a9b5b3fc5f0ac60c5d7269e45
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2025-01-30 09:34:23 -0500

    bump versionmeta

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ const (
 	VersionMajor = 1       // Major version component of the current release
 	VersionMinor = 14      // Minor version component of the current release
 	VersionPatch = 0       // Patch version component of the current release
-	VersionMeta  = "fh2.4" // Version metadata to append to the version string
+	VersionMeta  = "isoncurvefix-fh2.4" // Version metadata to append to the version string
 
 	FirehoseVersionMajor = 2
 	FirehoseVersionMinor = 4
