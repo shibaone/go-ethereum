05733b38c021162df36577a18d7f473240054421
Matthieu Vachon <matt@streamingfast.io>
2025-02-12 14:53:56 -0500

    Removed precompiles and PrepapreBlock as they were unused actually

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -144,9 +144,6 @@ type StateDB struct {
 	// Preimages occurred seen by VM in the scope of block.
 	preimages map[common.Hash][]byte
 
-	// Enabled precompile contracts
-	precompiles map[common.Address]struct{}
-
 	// Per-transaction access list
 	accessList *accessList
 
@@ -211,7 +208,6 @@ func New(root common.Hash, db Database, snaps *snapshot.Tree) (*StateDB, error) 
 		mutations:            make(map[common.Address]*mutation),
 		logs:                 make(map[common.Hash][]*types.Log),
 		preimages:            make(map[common.Hash][]byte),
-		precompiles:          make(map[common.Address]struct{}),
 		journal:              newJournal(),
 		accessList:           newAccessList(),
 		transientStorage:     newTransientStorage(),
@@ -1500,14 +1496,6 @@ func (s *StateDB) Prepare(rules params.Rules, sender, coinbase common.Address, d
 	}
 	// Reset transient storage at the beginning of transaction execution
 	s.transientStorage = newTransientStorage()
-}
-
-// PrepareBlock prepares the statedb for execution of a block. It tracks
-// the addresses of enabled precompiles for debugging purposes.
-func (s *StateDB) PrepareBlock(precompiles []common.Address) {
-	for _, addr := range precompiles {
-		s.precompiles[addr] = struct{}{}
-	}
 }
 
 // AddAddressToAccessList adds the given address to the access list
